18bfdae2310e1613da0a37f522701b277ff0a664
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2023-11-07 16:49:51 +0100

    article

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 "use client";
 
 import Search from "@/components/search/search";
+import Card from "@/components/articleCard/articleCard";
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
-    <div className={"page"}>
+    <div className={styles.page}>
       <Search />
+      <Card />
     </div>
   );
 }
--- src/components/articleCard/articleCard.js
+++ src/components/articleCard/articleCard.js
@@ -1,13 +1,48 @@
-"use client";
 import Image from "next/image";
+import styles from "./articleCard.module.css";
+import { playfair } from "@/utils/fonts";
 
-const Card = ({ title, content, button, image }) => {
+const articleData = [
+  {
+    title: "Selling",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    button: "Read More",
+    image: "/selling.jpg",
+  },
+  {
+    title: "Buying",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    button: "Read More",
+    image: "/buying.jpg",
+  },
+];
+
+const CardBuild = ({ title, content, button, image }) => {
   return (
-    <div className="blog-card">
+    <div className={styles.cardContainer}>
       <h2>{title}</h2>
       <p>{content}</p>
       <button>{button}</button>
+
       <Image src={image} alt={title} width={300} height={300} />
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className={styles.cardWrapper}>
+      {articleData.map((card, index) => (
+        <CardBuild
+          key={index}
+          title={card.title}
+          content={card.content}
+          button={card.button}
+          image={card.image}
+        />
+      ))}
     </div>
   );
 };
