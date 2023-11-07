e6210027276828dae4919f1f0c4e221341624f26
TiiaP <123360783+TiiaPitkanen@users.noreply.github.com>
2023-11-07 12:26:24 +0100

    search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -23,6 +23,62 @@ const Search = () => {
 					<button className={styles.searchButton}>Search Properties</button>
 					<button className={styles.searchButton}>Search Brokers</button>
 				</div>
+				<div className={styles.middleContainer}>
+					<div className={styles.midFirst}>
+						<div className={styles.radioWrapper}>
+							{' '}
+							<input type="radio" id="rent"></input>
+							<input type="radio" id="buy" name="buy"></input>{' '}
+						</div>
+						<div className={styles.idSearch}>
+							<input
+								type="text"
+								id="id"
+								name="search with id"
+								placeholder="Search with property ID"
+							></input>
+						</div>
+					</div>
+					<div className={styles.midSecond}>
+						<div className={styles.addressSearch}>
+							<input
+								type="text"
+								id="address"
+								name="address"
+								placeholder="Type city, postnumber or address"
+							></input>
+						</div>
+						<div className={styles.typeSelect}>
+							<select name="types" id="types">
+								<option value="" disabled selected>
+									Property Type
+								</option>
+								<option value="house">House</option>
+								<option value="apartment">Apartment</option>
+								<option value="summerhouse">Summerhouse</option>
+							</select>
+						</div>
+					</div>
+					<div className={styles.midThird}>
+						<div className={styles.price}>
+							<p>Price</p>
+							<input
+								type="number"
+								id="price"
+								name="price"
+								placeholder="min"
+							></input>
+							<p>000€ -</p>
+							<input
+								type="number"
+								id="price"
+								name="price"
+								placeholder="max"
+							></input>
+							<p>000€</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
