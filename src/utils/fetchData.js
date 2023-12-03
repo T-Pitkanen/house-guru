16dc31f0f0439a053439b5090fb565eae2bc876b
Tiia Pitkänen <tiia1.pitkanen@gmail.com>
2023-12-03 19:04:13 +0100

    -

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,8 @@
 // src/utils/fetchData.js
+
+/*
 export async function getBlogData() {
   const response = await fetch('/api/blog-posts');
   const data = await response.json();
   return data;
-}
+} */
